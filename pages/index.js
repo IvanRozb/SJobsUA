5c61519c3bc3c4116a3670296bb87db04d5eeea9
huan_rozb <ivanrozb4225@gmail.com>
2023-02-05 18:42:01 +0200

    starting building map

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Sidebar from "@/components/sidebar/sidebar";
+import MapWrapper from "@/components/map/map-wrapper";
 
 export default function Home() {
-  return <Sidebar />;
+  return (
+    <div style={{ display: "flex" }}>
+      <MapWrapper />
+      <Sidebar />
+    </div>
+  );
 }
