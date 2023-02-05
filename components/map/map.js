0b01f65bd038b5725974fa74f71a2c5767ad12ae
huan_rozb <ivanrozb4225@gmail.com>
2023-02-05 19:48:02 +0200

    added simple map

--- components/map/map.js
+++ components/map/map.js
@@ -1,3 +1,25 @@
-export default function Map() {
-  return <p>1</p>;
+import ReactMapGL from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useState } from "react";
+
+export default function Map(props) {
+  // const { vacancies } = props;
+
+  const [viewport, setViewport] = useState({
+    latitude: 48.3794,
+    longitude: 31.1656,
+    width: "100%",
+    height: "100%",
+  });
+
+  return (
+    <ReactMapGL
+      mapStyle={"mapbox://styles/mapbox/dark-v11"}
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+      minZoom={5.5}
+      maxZoom={15}
+      {...viewport}
+      onMove={(evt) => setViewport(evt.viewport)}
+    />
+  );
 }
