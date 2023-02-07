9bcdec326802b5d58e021ffc97e0141b2b7aec5f
huan_rozb <ivanrozb4225@gmail.com>
2023-02-07 20:31:59 +0200

    added server side fetching for markers

--- components/map/render-map.js
+++ components/map/render-map.js
@@ -4,22 +4,22 @@ import { createContext, useCallback, useState } from "react";
 
 import MarkerList from "@/components/map/ui/marker/marker-list";
 
-export const Context = createContext();
+export const Context = createContext(undefined);
 
-export default function RenderMap() {
-  // const { vacancies } = props;
+export default function RenderMap(props) {
+  const { vacancies } = props;
+
+  const center = { latitude: 48.3794, longitude: 31.1656 };
+  const latitudeBoundaries = [22.1, 44.14];
+  const longitudeBoundaries = [40.35, 52.38];
+  const boundaries = [latitudeBoundaries, longitudeBoundaries];
 
   const [cursor, setCursor] = useState("auto");
-  const [markers, setMarkers] = useState([
-    { id: 0, latitude: 48, longitude: 30, isActive: false, index: 0 },
-    { id: 1, latitude: 47, longitude: 30, isActive: false, index: 1 },
-  ]);
-
+  const [markers, setMarkers] = useState(vacancies);
   const [viewport, setViewport] = useState({
-    latitude: 48.3794,
-    longitude: 31.1656,
     width: "100%",
     height: "100%",
+    ...center,
   });
 
   const onViewportChangeHandler = useCallback((evt) => {
@@ -37,10 +37,7 @@ export default function RenderMap() {
         maxZoom={14}
         dragRotate={false}
         touchRotate={false}
-        maxBounds={[
-          [22.1, 44.14],
-          [40.35, 52.38],
-        ]}
+        maxBounds={boundaries}
         cursor={cursor}
         {...viewport}
         onMove={onViewportChangeHandler}
