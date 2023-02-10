9632e69b8d7f329d7c4005e5fdcbf4308453fa81
huan_rozb <ivanrozb4225@gmail.com>
2023-02-10 20:29:38 +0200

    map v1.1.0(changed approach cardinally)(useSupercluster approach)

--- components/map/ui/map-pin.js
+++ components/map/ui/map-pin.js
@@ -1,25 +1,39 @@
 import classes from "./map-pin.module.css";
+import { Marker } from "react-map-gl";
+import { useContext } from "react";
+import { Context } from "@/components/map/render-map";
 
-export default function MapPin() {
+export default function MapPin(props) {
+  const { longitude, latitude } = props.marker;
+  const { setSelectedMarker } = useContext(Context);
+
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      className={classes.map_pin}
-      viewBox="0 0 24 24"
+    <Marker
+      latitude={latitude}
+      longitude={longitude}
+      onClick={() => {
+        setSelectedMarker(props.marker);
+      }}
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-      ></path>
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-      ></path>
-    </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="black"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        className={classes.map_pin}
+        viewBox="0 0 24 24"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+        ></path>
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+        ></path>
+      </svg>
+    </Marker>
   );
 }
