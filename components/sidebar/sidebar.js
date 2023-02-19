a9cbe2292f2c1a26fcac2082df4451a0db3b94c8
huan_rozb <ivanrozb4225@gmail.com>
2023-02-19 13:22:39 +0200

    Added navbar with filters and moved existing filter functionality to it

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -1,22 +1,5 @@
 import classes from "./sidebar.module.css";
-import FilterList from "@/components/sidebar/filter/collections/filter-list";
 
 export default function Sidebar() {
-  return (
-    <div className={classes.sidebar}>
-      <FilterList
-        filters={[
-          "default",
-          "JS",
-          "php",
-          "ruby",
-          "python",
-          "java",
-          "pm",
-          "CSharp",
-          ".NET",
-        ]}
-      />
-    </div>
-  );
+  return <div className={classes.sidebar}></div>;
 }
