a0577777a8b8eccf61ded0de9ac5d02e0fc1e3a0
huan_rozb <ivanrozb4225@gmail.com>
2023-02-21 11:54:17 +0200

    Added clear filter functionality

--- components/navbar/location-filter/dropdown-content/clear-filter.js
+++ components/navbar/location-filter/dropdown-content/clear-filter.js
@@ -1,15 +1,18 @@
 import classesClear from "@/components/navbar/location-filter/dropdown-content/clear-filter.module.css";
 import classesItem from "@/components/navbar/location-filter/collections/location-item.module.css";
 import Ripples from "react-ripples";
+import Link from "next/link";
 
-export default function ClearFilter() {
+export default function ClearFilter({ filter }) {
   return (
-    <Ripples className={classesItem.location_ripple}>
-      <div
-        className={`${classesItem.location_item} ${classesClear.location_clear}`}
-      >
-        Clear filter
-      </div>
-    </Ripples>
+    <Link href={{ pathname: `/${filter}` }}>
+      <Ripples className={classesItem.location_ripple}>
+        <div
+          className={`${classesItem.location_item} ${classesClear.location_clear}`}
+        >
+          Clear filter
+        </div>
+      </Ripples>
+    </Link>
   );
 }
