457ad98ea61e237f6e75ceb10185feb452003d07
huan_rozb <ivanrozb4225@gmail.com>
2023-02-26 17:13:46 +0200

    Added more stylization to sidebar

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -6,17 +6,17 @@ export default function Sidebar({vacancies}) {
     return <div className={classes.sidebar}>
         {vacancies?.map(vacancy => <div key={vacancy.id} className={classes.vacancy}>
             <div className={classes.vacancy_introduction}>
-                <div><Image src={'https://via.placeholder.com/140x80'} width={140} height={80} alt={'CompanyTitle'}></Image></div>
-                <div>
-                    <h3>Title_Job</h3>
+                <div className={classes.vacancy_image}><Image src={'https://via.placeholder.com/120x65'} width={120} height={65} alt={'CompanyTitle'}></Image></div>
+                <div className={classes.vacancy_inner}>
+                    <h3 className={classes.vacancy_title}>Title_Job</h3>
                     <div className={classes.vacancy_location}>
-                        <p>Company_title</p>
+                        <p className={classes.vacancy_company_title}>Company_title</p>
                         <p>Region</p>
                     </div>
                 </div>
             </div>
             <div className={classes.vacancy_info}>
-                <p>Salary</p>
+                <p className={classes.vacancy_salary}>Salary</p>
                 <time>Date_Time</time>
             </div>
         </div>)}
