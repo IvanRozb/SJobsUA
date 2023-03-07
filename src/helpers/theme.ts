25b497efdb6cc3d5e528a62dcf208529158097b5
huan_rozb <ivanrozb4225@gmail.com>
2023-03-07 20:17:01 +0200

    themes(palette)

--- src/helpers/theme.ts
+++ src/helpers/theme.ts
@@ -1,3 +1,15 @@
 import { createTheme } from "@mui/material";
 
-export const theme = createTheme({});
+export const theme = createTheme({
+	palette: {
+		common:{
+			white: 'rgba(255,255,255,0.1)',
+		},
+		primary: {
+			main: "#2c2c2c"
+		},
+		secondary: {
+			main: "#202020",
+		}
+	}
+});
