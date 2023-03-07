1630d658fa3e512c0f6f6ba3b441581a8d8e112f
huan_rozb <ivanrozb4225@gmail.com>
2023-03-07 19:42:24 +0200

    added simple appbar

---
+++ src/modules/header/index.tsx
@@ -0,0 +1,10 @@
+import { AppBar, Toolbar } from "@mui/material";
+import React from "react";
+
+const Header: React.FC = () => {
+	return <AppBar>
+		<Toolbar>SJobsUA</Toolbar>
+	</AppBar>
+};
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Header from "@/modules/header";
 
 const Home: React.FC = () => {
-	return <></>;
+	return <Header/>;
 };
 
 export default Home;
