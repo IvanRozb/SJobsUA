785ea767e7365d56212002b4c8ba51e4563dede0
huan_rozb <ivanrozb4225@gmail.com>
2023-03-08 11:58:15 +0200

    added tabs

--- src/helpers/theme.ts
+++ src/helpers/theme.ts
@@ -2,20 +2,31 @@ import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
 	palette: {
-		common:{
-			white: 'rgba(255,255,255,0.1)',
+		common: {
+			white: "rgba(255,255,255,0.8)"
 		},
 		primary: {
 			main: "#2c2c2c"
 		},
 		secondary: {
-			main: "#202020",
+			main: "#202020"
 		},
+		text: {
+			secondary: "rgba(255,255,255,0.8)"
+		}
 	},
 	typography: {
+		fontFamily: "Open Sans, sans-serif",
 		h1: {
-			fontSize: '1.65rem',
+			fontSize: "1.65rem",
 			fontWeight: "700"
+		},
+		h2: {
+			fontSize: "0.875rem"
+		},
+		button: {
+			textTransform: "none",
+			fontWeight: 700
 		}
 	}
 });
--- src/modules/header/index.tsx
+++ src/modules/header/index.tsx
@@ -1,9 +1,18 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 import React from "react";
+import classes from '@/modules/header/header.module.scss'
 
 const Header: React.FC = () => {
 	return <AppBar>
-		<Toolbar><Typography variant="h1">SJobsUA</Typography></Toolbar>
+		<Toolbar>
+			<Typography variant="h1">SJobsUA</Typography>
+			<Typography variant="h2" className={classes.subtitle}>Job Board for tech industry in Ukraine</Typography>
+			<Tabs className={classes.tabs}>
+				<Tab label="Offers" disableTouchRipple/>
+				<Tab className={classes.post_tab} label="Post a Job"/>
+				<Tab className={classes.sign_tab} label="Sign in"/>
+			</Tabs>
+		</Toolbar>
 	</AppBar>;
 };
 
